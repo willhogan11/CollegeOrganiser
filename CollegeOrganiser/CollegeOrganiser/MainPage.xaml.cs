85a4cef3922db5ac710576655d585284cf200437
Will Hogan <willhogan11@hotmail.com>
2016-09-27 23:28:53 +0100

    Link completed between MainPage and MenuPage, system Back button working on Menu page

--- CollegeOrganiser/CollegeOrganiser/MainPage.xaml.cs
+++ CollegeOrganiser/CollegeOrganiser/MainPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +16,18 @@ namespace CollegeOrganiser
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        // Closes this MainPage when the below function is called to open the MenuPage
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        // Navigates to the Menu Page when Tapped
+        private void Page_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MenuPage));
         }
     }
 }
