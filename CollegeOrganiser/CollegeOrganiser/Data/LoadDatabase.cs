10527593bec93db6e0d96669e7ae613f2a12bf88
Will Hogan <willhogan11@hotmail.com>
2016-09-29 23:56:19 +0100

    Installing SQLite, Preparing LoadDatabase cs file

---
+++ CollegeOrganiser/CollegeOrganiser/Data/LoadDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace CollegeOrganiser.Data
+{
+    public class LoadDatabase
+    {
+        // Instance Variables that are required to Setup a path and a new sqlite Database connection
+        private string path;
+        // private SQLite.Net.SQLiteConnection conn;
+
+        public LoadDatabase()
+        {
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.collegeOrganiser");
+        }
+
+        private void dbConnection()
+        {
+            // conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        }
+    }
+}
