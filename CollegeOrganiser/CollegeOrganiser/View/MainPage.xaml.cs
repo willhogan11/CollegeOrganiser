a6fd716e84365403efbfd98a0c158a716846f80a
Will Hogan <willhogan11@hotmail.com>
2016-11-03 17:46:15 +0000

    Added Launch Page, changed background and Added Menu page that now links together on tapped

---
+++ CollegeOrganiser/CollegeOrganiser/View/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+using CollegeOrganiser.View;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CollegeOrganiser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Closes this MainPage when the below function is called to open the MenuPage
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        // Navigates to the Menu Page when Tapped
+        private void Page_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MenuPage));
+        }
+    }
+}
